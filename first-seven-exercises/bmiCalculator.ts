255df94ffd1c33d5074b246cf00b178139869e3d
Moritz <22394314+movd@users.noreply.github.com>
2020-05-28 18:31:25 +0200

    Finish Ex. 9.3 Command line

--- first-seven-exercises/bmiCalculator.ts
+++ first-seven-exercises/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface ParsedValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): ParsedValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
 type Result = string;
 
 const calculateBmi = (weight: number, height: number): Result => {
@@ -8,24 +27,29 @@ const calculateBmi = (weight: number, height: number): Result => {
   switch (true) {
     // case 18.5 > bmi && bmi < 25:
     case bmi < 15:
-      return "Very severely underweight";
+      return 'Very severely underweight';
     case bmi >= 15 && bmi < 16:
-      return "Severely underweight";
+      return 'Severely underweight';
     case bmi >= 16 && bmi < 18.5:
-      return "Underweight";
+      return 'Underweight';
     case bmi >= 18.5 && bmi < 25:
-      return "Normal (healthy weight)";
+      return 'Normal (healthy weight)';
     case bmi >= 25 && bmi < 30:
-      return "Overweight";
+      return 'Overweight';
     case bmi >= 30 && bmi < 35:
-      return "bese Class I (Moderately obese)";
+      return 'bese Class I (Moderately obese)';
     case bmi >= 35 && bmi < 40:
-      return "Obese Class II (Severely obese)";
+      return 'Obese Class II (Severely obese)';
     case bmi >= 40:
-      return "Obese Class III (Very severely obese)";
+      return 'Obese Class III (Very severely obese)';
     default:
-      return "N/A";
+      return 'N/A';
   }
 };
 
-console.log(calculateBmi(74, 180));
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(weight, height));
+} catch (error) {
+  console.log('Error, something bad happened, message: ', error.message);
+}
--- first-seven-exercises/exerciseCalculator.ts
+++ first-seven-exercises/exerciseCalculator.ts
@@ -1,3 +1,26 @@
+interface ParsedExerciseValues {
+  target: number;
+  weeklyExercise: Array<number>;
+}
+
+// npm run calculateExercises 2 1 0 2 4.5 0 3 1 0 4
+const parseExerciseArguments = (args: Array<string>): ParsedExerciseValues => {
+  if (args.length < 2) throw new Error('Not enough arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    // Remove first 3 elems using ES6 spread
+    const [, , , ...firstThreeArgsRemoved] = args;
+    const weeklyExercise = firstThreeArgsRemoved.map((e) => Number(e));
+
+    return {
+      target: Number(args[2]),
+      weeklyExercise,
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
 interface CalculateExercise {
   periodLength: number;
   trainingDays: number;
@@ -19,17 +42,17 @@ const calculateExercises = (
 
   if (average < target * 0.5) {
     rating = 1;
-    ratingDescription = "do more next week";
+    ratingDescription = 'do more next week';
   }
 
   if (average >= target * 0.5 && average < target * 0.999999) {
     rating = 2;
-    ratingDescription = "not too bad but could be better";
+    ratingDescription = 'not too bad but could be better';
   }
 
   if (average >= target) {
     rating = 3;
-    ratingDescription = "very good keep it going next week";
+    ratingDescription = 'very good keep it going next week';
   }
 
   return {
@@ -43,4 +66,10 @@ const calculateExercises = (
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  const { target, weeklyExercise } = parseExerciseArguments(process.argv);
+  // console.log(target, weeklyExercise);
+  console.log(calculateExercises(weeklyExercise, target));
+} catch (error) {
+  console.log('Error, something bad happened, message: ', error.message);
+}
