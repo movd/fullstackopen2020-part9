b1cedc9ac6faeb16bcb7aaa61e65cd5e4b10de6a
Moritz <22394314+movd@users.noreply.github.com>
2020-06-01 16:22:12 +0200

    Finish Ex. 9.10: GET /api/diagnoses

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong yours truly Patientor');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,13 @@
+import diagnosesData from '../../data/diagnoses.json';
+
+import { Diagnose } from '../types';
+
+const diagnoses: Array<Diagnose> = diagnosesData as Array<Diagnose>;
+
+const getDiagnoses = (): Array<Diagnose> => {
+  return diagnoses;
+};
+
+export default {
+  getDiagnoses,
+};
